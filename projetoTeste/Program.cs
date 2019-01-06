c595a35afc582f0576026455f1ff011bc7f65cc2
Nuno Barros <nunoaparros@gmail.com>
2019-01-06 22:25:30 +0000

    Acrescentada a pergunta sobre como voce esta

--- projetoTeste/Program.cs
+++ projetoTeste/Program.cs
@@ -12,6 +12,7 @@ namespace projetoTeste
         {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
         }
