2f05cc3bcfbc43906d6ff55f8af2854459d50a81
Nuno Barros <nunoapbarros@gmail.com>
2019-01-12 19:47:24 +0000

    Acrescentadas as linhas de traços

--- projetoTeste/Program.cs
+++ projetoTeste/Program.cs
@@ -10,9 +10,12 @@ namespace projetoTeste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("----------------------------");
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("----------------------------");
+
 
             Console.ReadLine();
         }
